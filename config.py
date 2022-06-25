43cd8b04dc14805385e799541b6e807b3739720c
Stanislav <107346041+stan6x@users.noreply.github.com>
2022-06-25 19:02:28 +0600

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+SESSION_NAME = "session"
+API_ID = ""
+API_HASH = ""
+INTERVAL = 20
+CHANNEL1 = [""]
+CHANNEL2 = ""
---
+++ main.py
@@ -0,0 +1,20 @@
+import asyncio
+
+from telethon import TelegramClient
+
+import config
+
+client = TelegramClient(config.SESSION_NAME, config.API_ID, config.API_HASH)
+
+
+async def main():
+    for channel in config.CHANNEL1:
+        print(channel)
+        async for message in client.iter_messages(channel):
+            print(message)
+            await client.send_message(config.CHANNEL2, message)
+            await asyncio.sleep(config.INTERVAL)
+
+
+with client:
+    client.loop.run_until_complete(main())
