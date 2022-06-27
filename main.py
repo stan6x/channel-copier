562053876aacb6edba19f726026a426d3225121d
Stanislav <107346041+stan6x@users.noreply.github.com>
2022-06-27 09:58:54 +0600

    bugfix

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import asyncio
-
-from telethon import TelegramClient
-
 import config
+from telethon import TelegramClient
 
 client = TelegramClient(config.SESSION_NAME, config.API_ID, config.API_HASH)
 
@@ -10,8 +8,9 @@ client = TelegramClient(config.SESSION_NAME, config.API_ID, config.API_HASH)
 async def main():
     for channel in config.CHANNEL1:
         async for message in client.iter_messages(channel, reverse=True):
-            await client.send_message(config.CHANNEL2, message)
-            await asyncio.sleep(config.INTERVAL)
+            if not message.action:
+                await client.send_message(config.CHANNEL2, message)
+                await asyncio.sleep(config.INTERVAL)
 
 
 with client:
