d21be4689c0d4c9ad36188fbdacc4c2a1010cbc0
Stanislav <107346041+stan6x@users.noreply.github.com>
2022-06-27 11:41:34 +0600

    bugfix

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import asyncio
-import config
+
 from telethon import TelegramClient
+from telethon.tl.types import MessageMediaUnsupported
+
+import config
 
 client = TelegramClient(config.SESSION_NAME, config.API_ID, config.API_HASH)
 
@@ -8,7 +11,7 @@ client = TelegramClient(config.SESSION_NAME, config.API_ID, config.API_HASH)
 async def main():
     for channel in config.CHANNEL1:
         async for message in client.iter_messages(channel, reverse=True):
-            if not message.action:
+            if not message.action and not message.media == MessageMediaUnsupported():
                 await client.send_message(config.CHANNEL2, message)
                 await asyncio.sleep(config.INTERVAL)
 
